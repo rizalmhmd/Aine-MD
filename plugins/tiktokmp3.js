0c83b0f264f4826704f2b726aee1e771ab8f5a5f
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-01 08:58:28 +0700

    Update tiktokmp3.js

--- plugins/tiktokmp3.js
+++ plugins/tiktokmp3.js
@@ -1,16 +1,14 @@
-                if (!text) throw 'Masukkan Query Link!'
-                m.reply(mess.wait)
-                let anu = await fetchJson(api('zenz', '/downloader/musically', { url: text }, 'apikey'))
-                let buttons = [
-                    {buttonId: `tiktoknowm ${text}`, buttonText: {displayText: '► No Watermark'}, type: 1},
-                    {buttonId: `tiktokwm ${text}`, buttonText: {displayText: '► With Watermark'}, type: 1}
-                ]
-                let buttonMessage = {
-                    text: `Download From ${text}`,
-                    footer: 'Press The Button Below',
-                    buttons: buttons,
-                    headerType: 2
-                }
-                let msg = await xavior.sendMessage(m.chat, buttonMessage, { quoted: m })
-                xavior.sendMessage(m.chat, { audio: { url: anu.result.audio }, mimetype: 'audio/mpeg'}, { quoted: msg })
-            }
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'Uhm..url nya mana?'
+let anu = await fetchJson(api('zenz', '/downloader/musically', { url: text }, 'apikey'))
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+await conn.sendMessage(m.chat, { audio: { url: anu.result.audio }, mimetype: 'audio/mpeg'}, { quoted: msg })
+
+
+
+handler.help = ['tiktokmp3' <url>']
+handler.tags = ['downloader']
+handler.command = /^tiktokmp3$/i
