1f9086455d1303f283fff453557d8aa4151b8461
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-01 08:46:05 +0700

    Create tiktokmp3.js

---
+++ plugins/tiktokmp3.js
@@ -0,0 +1,17 @@
+case 'tiktokmp3': case 'tiktokaudio': {
+                if (!text) throw 'Masukkan Query Link!'
+                m.reply(mess.wait)
+                let anu = await fetchJson(api('zenz', '/downloader/musically', { url: text }, 'apikey'))
+                let buttons = [
+                    {buttonId: `tiktoknowm ${text}`, buttonText: {displayText: '► No Watermark'}, type: 1},
+                    {buttonId: `tiktokwm ${text}`, buttonText: {displayText: '► With Watermark'}, type: 1}
+                ]
+                let buttonMessage = {
+                    text: `Download From ${text}`,
+                    footer: 'Press The Button Below',
+                    buttons: buttons,
+                    headerType: 2
+                }
+                let msg = await xavior.sendMessage(m.chat, buttonMessage, { quoted: m })
+                xavior.sendMessage(m.chat, { audio: { url: anu.result.audio }, mimetype: 'audio/mpeg'}, { quoted: msg })
+            }
